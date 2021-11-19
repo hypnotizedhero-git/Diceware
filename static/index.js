f2204bed709b5eb0d64f1edf00663138beb316d2
hypnotizedher-git <ridhamkaria@gmail.com>
2021-11-20 01:30:37 +0530

    First Commit.

--- static/index.js
+++ static/index.js
@@ -1,3 +1,32 @@
-iw=window.innerWidth
-ih=window.innerHeight
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 55, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const loader = new GLTFLoader();
+
+loader.load('static/dice.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+camera.position.z = 5;
+
+var animate = function () {
+  requestAnimationFrame( animate );
+
+//   gltf.scene.rotation.x += 0.01;
+//   gltf.scene.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+};
+
+animate();
